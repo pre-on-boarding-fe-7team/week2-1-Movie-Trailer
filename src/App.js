e658f8edba885575f929d286e493185e215cac97
Malza0408 <parkjh04080@gmail.com>
2022-09-07 01:06:42 +0900

    Feat: 추가 UpComing 컴포넌트 라우팅

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { ROUTE } from './common/utils/constant';
 import Main from './pages/Main/Main';
 import NotFound from './pages/NotFound/NotFound';
+import UpComing from './pages/UpComing/UpComing';
 
 function App() {
   return (
     <Routes>
       <Route path={ROUTE.MAIN} element={<Main />} />
+      <Route path={ROUTE.UP_COMING} element={<UpComing />} />
       <Route path={ROUTE.NOT_FOUND} element={<NotFound />} />
     </Routes>
   );
