b40f554e3e009764c3fb9479b925f1ef89963bca
Malza0408 <parkjh04080@gmail.com>
2022-09-07 00:54:59 +0900

    Refactor: 수정 api get 함수
    TMDB api를 사용하기 위한 setting. Authorization와 Content-Type 추가

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,12 @@ const baseUrl = process.env.REACT_APP_SERVER_URL;
 
 const get = async endpoint => {
   const url = baseUrl + endpoint;
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  });
   if (!res.ok) {
     throw new Error(`${res.status.toString()} Error 인한 요청 실패!`);
   }
