602eb0ff76802d2bbba61a71c2bba879498d048e
Malza0408 <parkjh04080@gmail.com>
2022-09-07 02:41:56 +0900

    Feat: 추가 youtube link 연결

--- src/pages/Upcoming/Upcoming.jsx
+++ src/pages/Upcoming/Upcoming.jsx
@@ -4,11 +4,17 @@ import { Container } from './Upcoming.style';
 
 function UpComing() {
   const [upcomingMovieList, setUpcomingMovieList] = useState([]);
+  const [movieSrc, setMovieSrc] = useState('');
   useEffect(() => {
     const getMovie = async () => {
       const res = await get(`/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=ko`);
-      console.info(res.results);
       setUpcomingMovieList(res.results);
+      console.info(res.results);
+      const movie = await get(
+        `/movie/${539681}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=ko`
+      );
+      console.info(movie.results);
+      setMovieSrc(movie.results[0].key);
     };
     getMovie();
   }, []);
@@ -18,6 +24,8 @@ function UpComing() {
         <Fragment key={idx}>
           <div>{movie.title}</div>
           <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+          <a href={`https://www.youtube.com/watch?v=${movieSrc}`}>여기로 가세요</a>
+          <p>{movieSrc}</p>
         </Fragment>
       ))}
     </Container>
