07c423421d321fa1293277d0b44df6ad2cca82f9
Sujin Choi <hissujinc@gmail.com>
2022-09-07 14:00:26 +0900

    Comment: 삭제 주석처리 된 api 불러오는 이전 버전 삭제

--- src/pages/TopRatedList/TopRatedList.jsx
+++ src/pages/TopRatedList/TopRatedList.jsx
@@ -1,28 +1,10 @@
-// import { useState, useEffect } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import useFetchData from '../../hooks/useFetchData';
-// import { get } from '../../api/api';
 import TopRated from './TopRated';
 import { Ul } from './TopRatedList.style.js';
 
 export default function TopRatedList() {
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [topRatedList, setTopRatedList] = useState([]);
-
-  // const getProductsList = async () => {
-  //   const res = await get('/movie/top_rated');
-  //   setTopRatedList(res.results);
-  // };
-  // useEffect(() => {
-  //   getProductsList();
-  //   setIsLoading(false);
-  // }, []);
-
-  // const handleClose = () => {
-  //   setIsLoading(false);
-  // };
-
   const { data, isLoading } = useFetchData('/movie/top_rated');
 
   return (
