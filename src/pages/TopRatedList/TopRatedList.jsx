94627f89c0dc683dbfcc6ac083a6b722f5faec71
Sujin Choi <hissujinc@gmail.com>
2022-09-07 14:36:41 +0900

    Refactor: 변경 공통 API 로 변경

--- src/pages/TopRatedList/TopRatedList.jsx
+++ src/pages/TopRatedList/TopRatedList.jsx
@@ -1,32 +1,40 @@
+import { useQuery } from 'react-query';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-import useFetchData from '../../hooks/useFetchData';
+import { getMovieTopRated } from '../../api/api';
 import TopRated from './TopRated';
 import { Ul } from './TopRatedList.style.js';
 
 export default function TopRatedList() {
-  const { data, isLoading } = useFetchData('/movie/top_rated');
+  const { isLoading: detailLoading, data: TopRatedList } = useQuery('TopRatedList', () =>
+    getMovieTopRated()
+  );
+
+  if (detailLoading === true) {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+        open={detailLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
+  }
 
   return (
     <>
-      {isLoading ? (
-        <Backdrop sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }} open={isLoading}>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      ) : (
-        <Ul>
-          {data.results.map(movie => {
-            return (
-              <TopRated
-                key={movie.id}
-                title={movie.title}
-                posterImg={movie.poster_path}
-                average={movie.vote_average}
-              />
-            );
-          })}
-        </Ul>
-      )}
+      <Ul>
+        {TopRatedList.map(movie => {
+          return (
+            <TopRated
+              key={movie.id}
+              title={movie.title}
+              posterImg={movie.poster_path}
+              average={movie.vote_average}
+            />
+          );
+        })}
+      </Ul>
     </>
   );
 }
