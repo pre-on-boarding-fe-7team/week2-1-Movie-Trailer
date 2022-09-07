c637ce5f1c1a5c5f0e46af50031bc47504f0b74c
cssong <a1373052011@gmail.com>
2022-09-07 20:18:03 +0900

    Feat: 변경 useQuery에 key 추가
    key를 추가하여 query 값이 변경될 때마다 재조회하도록 설정

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,20 +1,23 @@
-import { ImageListItem, ImageListItemBar } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
 import { getSearchMovie } from '../../api/api.js';
-import { getImage } from '../../common/utils/image.js';
-import { Bold, Box, MovieImageList, Title } from './Search.style.js';
-import Star from './Star/Star.jsx';
+import { Bold, Box, Title } from './Search.style.js';
+import MovieList from './MovieList/MovieList.jsx';
+import { Backdrop, CircularProgress } from '@mui/material';
 
 const Search = () => {
   const { state } = useLocation();
   const query = state ? state.query : '';
-
-  const { data: searchMovie, status } = useQuery('searchMovie', () => getSearchMovie(query));
+  const { data: movies, status } = useQuery(['searchMovie', query], () => getSearchMovie(query));
 
   if (status === 'loading') {
-    return <>loading...</>;
+    // return <>loading...</>;
+    return (
+      <Backdrop sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }} open={true}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
   }
 
   if (status === 'error') {
@@ -23,19 +26,12 @@ const Search = () => {
 
   return (
     <Box>
-      {searchMovie?.length > 0 ? (
+      {movies?.length > 0 ? (
         <>
           <Title variant="h6">
             "<Bold>{query}</Bold>"에 대한 검색 결과
           </Title>
-          <MovieImageList cols={5} gap={8}>
-            {searchMovie?.map(({ id, title, vote_average, poster_path }) => (
-              <ImageListItem key={id}>
-                <img src={getImage(poster_path)} alt={title} loading="lazy" />
-                <ImageListItemBar title={title} subtitle={<Star value={vote_average / 2} />} />
-              </ImageListItem>
-            ))}
-          </MovieImageList>
+          <MovieList movies={movies} />
         </>
       ) : (
         <p>입력하신 "{query}"에 대한 검색 결과가 없습니다.</p>
