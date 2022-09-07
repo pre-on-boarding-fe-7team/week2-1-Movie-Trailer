4e9005615b0a43c22784f366d469cbeb150bb7be
Sujin Choi <hissujinc@gmail.com>
2022-09-07 21:27:09 +0900

    Refactor: 변경 영화 리스트 컴포넌트 위치
    공통 컴포넌트이기 때문에 컴포넌트 폴더로 이동

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,16 @@
+import MovieItem from './MovieItem';
+import { Container, MovieImageList } from './MovieList.style';
+
+const MovieList = ({ movies }) => {
+  return (
+    <Container>
+      <MovieImageList gap={12}>
+        {movies?.map(({ id, title, vote_average, poster_path }) => (
+          <MovieItem key={id} title={title} vote_average={vote_average} poster_path={poster_path} />
+        ))}
+      </MovieImageList>
+    </Container>
+  );
+};
+
+export default MovieList;
