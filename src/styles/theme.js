9064402826585044a38ae74fe8c33ee7d2de5f90
ezn6 <kcourtney@naver.com>
2022-09-07 14:59:01 +0900

    Design: 추가 theme colors
    공통적으로 쓸 색상 theme을 추가하였다

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -5,6 +5,13 @@ const theme = {
     pc: '(min-width: 1100px)',
     mobile: '(min-width: 768px)',
   },
+
+  colors: {
+    black: '#100720',
+    purple: '#31087B',
+    pink: '#FA2FB5',
+    yellow: '#FFC23C',
+  },
 };
 
 export { theme };
