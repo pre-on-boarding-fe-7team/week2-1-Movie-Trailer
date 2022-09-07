4c84585257a93a6f64414777e27de2cb7febc9bd
ezn6 <kcourtney@naver.com>
2022-09-07 14:19:46 +0900

    Design: 추가 레이아웃
    영화상세페이지의 레이아웃 css를 만들었다

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,75 +1,78 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import axios from 'axios';
+import { Background, Section, InfoDiv, Video, Poster } from './MovieDetail.style';
+import { CircularProgress } from '@mui/material';
+import { getMovieDetail, getMovieVideo } from '../../api/api';
 
 const MovieDetail = () => {
   const { id } = useParams();
-
-  const httpClient = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}/movie`,
-    params: { api_key: process.env.REACT_APP_API_KEY },
-  });
-
-  const getMovieDetail = async () => {
-    const response = await httpClient.get(`${id}`, {
-      params: {
-        language: 'en-US',
-      },
-    });
-    return response.data;
-  };
-
-  const getMovieVideo = async () => {
-    const response = await httpClient.get(`${id}/videos`);
-    return response.data.results[0].key;
-  };
 
   // const results = useQueries([
   //   { queryKey: 'movieDetail', queryFn: getMovieDetail },
   //   { queryKey: 'movieVideo', queryFn: getMovieVideo },
   // ]);
 
-  const { isLoading: detailLoading, data: movieDetail } = useQuery('movieDetail', getMovieDetail);
-  const { isLoading: videoLoading, data: movieVideo } = useQuery('movieVideo', getMovieVideo);
+  const { isLoading: detailLoading, data: movieDetail } = useQuery('movieDetail', () =>
+    getMovieDetail(id)
+  );
+  const { isLoading: videoLoading, data: movieVideo } = useQuery('movieVideo', () =>
+    getMovieVideo(id)
+  );
 
   if ((detailLoading || videoLoading) === true) {
-    return <span>Loading...</span>;
+    return (
+      <div style={{ backgroundColor: 'black', width: '100vw', height: '100vh', color: 'white' }}>
+        <CircularProgress />
+        Loading...
+      </div>
+    );
   }
 
   return (
-    <>
-      <button>test</button>
-      <img
-        src={`${process.env.REACT_APP_IMG_URL}/w500/${movieDetail.poster_path}`}
+    <Section>
+      <div>
+        <Background
+          src={`${process.env.REACT_APP_IMG_URL}/original/${movieDetail.backdrop_path}`}
+          alt="back_img"
+        />
+        <InfoDiv>
+          <div>제목 : {movieDetail.original_title}</div>
+          <div>별점 : {movieDetail.vote_average} / 10</div>
+          <div>제작연도 : {movieDetail.release_date.split('-')[0]}</div>
+          <div>
+            장르 :
+            {movieDetail.genres.map(i => (
+              <span key={i.name}> {i.name}</span>
+            ))}
+          </div>
+        </InfoDiv>
+      </div>
+
+      <div>
+        {movieVideo && (
+          <Video
+            title={movieDetail.original_title}
+            id="ytplayer"
+            type="text/html"
+            width="720"
+            height="405"
+            src={`https://www.youtube.com/embed/${movieVideo}?autoplay=1&mute=1`}
+            frameBorder="0"
+            allowFullScreen
+          ></Video>
+        )}
+
+        <div style={{ color: 'white', marginLeft: '20rem', marginTop: '2rem' }}>
+          {movieDetail.overview}
+        </div>
+      </div>
+
+      <Poster
+        src={`${process.env.REACT_APP_IMG_URL}/original/${movieDetail.poster_path}`}
         alt="poster_img"
       />
-      <img
-        src={`${process.env.REACT_APP_IMG_URL}/w500/${movieDetail.backdrop_path}`}
-        alt="back_img"
-      />
-      <div>제목 : {movieDetail.original_title}</div>
-      <div>별점 : {movieDetail.vote_average}</div>
-      <div>제작연도 : {movieDetail.release_date.split('-')[0]}</div>
-      <div>
-        장르 :
-        {movieDetail.genres.map(i => (
-          <span key={i.name}> {i.name}</span>
-        ))}
-      </div>
-      {movieVideo && (
-        <iframe
-          title={movieDetail.original_title}
-          id="ytplayer"
-          type="text/html"
-          width="720"
-          height="405"
-          src={`https://www.youtube.com/embed/${movieVideo}?autoplay=1&mute=1`}
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      )}
-    </>
+    </Section>
   );
 };
 
