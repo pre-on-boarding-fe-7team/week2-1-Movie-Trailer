16cf3d62634591c00e07afa40745e670309a2551
Sujin Choi <hissujinc@gmail.com>
2022-09-07 21:30:25 +0900

    Design: 추가 리스트 감싼 컨테이너 여백 추가

--- src/pages/TopRatedList/TopRatedList.style.js
+++ src/pages/TopRatedList/TopRatedList.style.js
@@ -1,15 +1,7 @@
+import { Box as MuiBox } from '@mui/material';
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  ${props => props.theme.common.flexCenter};
+export const Container = styled(MuiBox)`
   background: ${props => props.theme.colors.black};
-`;
-
-export const Ul = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  grid-gap: ${props => props.theme.paddings.xl};
-  grid-row-gap: 2rem;
-  max-width: ${props => props.theme.maxWidth};
-  width: 100%;
+  padding: 40px 20px;
 `;
