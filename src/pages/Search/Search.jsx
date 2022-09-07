f76a8a316909ceb4bfafaebf00719fcc5b0fb3fd
cssong <a1373052011@gmail.com>
2022-09-07 17:56:58 +0900

    Feat: 변경 getImage 적용
    대체이미지 적용을 위한 getImage 사용

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -3,15 +3,18 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { get } from '../../api/api.js';
+import { getImage } from '../../common/utils/image.js';
 import { Bold, Box, MovieImageList, Title } from './Search.style.js';
 import Star from './Star/Star.jsx';
 
 const Search = () => {
   const [movies, setMovies] = useState([]);
-  const location = useLocation();
-  const query = location.state.query;
+  const { state } = useLocation();
+  const query = state ? state.query : '';
 
   useEffect(() => {
+    if (!query) return;
+
     const searchMovies = async () => {
       const url = `/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=ko&query=${query}`;
       const res = await get(url);
@@ -27,14 +30,10 @@ const Search = () => {
           <Title variant="h6">
             "<Bold>{query}</Bold>"에 대한 검색 결과
           </Title>
-          <MovieImageList cols={5} gap={8}>
+          <MovieImageList gap={8}>
             {movies.map(({ id, title, vote_average, poster_path }) => (
               <ImageListItem key={id}>
-                <img
-                  src={`${process.env.REACT_APP_IMAGE_URL}${poster_path}`}
-                  alt={title}
-                  loading="lazy"
-                />
+                <img src={getImage(poster_path)} alt={title} loading="lazy" />
                 <ImageListItemBar title={title} subtitle={<Star value={vote_average / 2} />} />
               </ImageListItem>
             ))}
