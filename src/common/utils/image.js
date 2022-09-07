6ce7140ec52807bf1713ec0f4a6c0cc90cadd227
cssong <a1373052011@gmail.com>
2022-09-07 17:27:26 +0900

    Feat: 추가 image 공통
    getImage 함수 추가
    - 파라미터로 포스터 경로 전달 받고 전달받은 파라미터가 없는 경우 대체이미지 출력

---
+++ src/common/utils/image.js
@@ -0,0 +1,4 @@
+export const getImage = poster_path =>
+  poster_path
+    ? `${process.env.REACT_APP_IMAGE_URL}${poster_path}`
+    : 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg';
