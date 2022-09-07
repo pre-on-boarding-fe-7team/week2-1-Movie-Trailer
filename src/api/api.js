5c3a7cd22a3448a94a98649a35d686dbafba278a
Sujin Choi <hissujinc@gmail.com>
2022-09-07 13:32:50 +0900

    Feat: 추가 axios
    fetch에서 axios 로 변경을 위해 axios 추가하고, api 호출하는 useHooks 생성

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,14 @@
+import axios from 'axios';
 const baseUrl = process.env.REACT_APP_SERVER_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const get = async endpoint => {
-  const url = baseUrl + endpoint;
-  const res = await fetch(url);
-  if (!res.ok) {
+  const url = baseUrl + endpoint + '?api_key=' + apiKey;
+  const res = await axios(url);
+  if (res.status !== 200) {
     throw new Error(`${res.status.toString()} Error 인한 요청 실패!`);
   }
-  const result = await res.json();
-
+  const result = await res.data;
   return result;
 };
 
